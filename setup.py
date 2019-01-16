19fe725c52c0fbc5aa2291f3a6a63df8a62d6ec7
mathieu_barbier <mathieu.barbier@renault.com>
2019-01-16 11:23:41 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='I want to know more about rl',
+    author='Mathieu Barbier',
+    license='',
+)
